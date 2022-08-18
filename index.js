226554051a2bd94ff38ddc52f1cdf56179a3e6cf
Doreen <doreenlulu97@gmail.com>
2022-08-18 22:48:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let multiply, num1, num2
+num1 = 31;
+num2 = 2;
+multiply =num1 * num2; //=>62
+
+let random = Math.floor((Math.random() * 10) + 1);
+
+let mod, num3, num4
+num3 =24;
+num4 = 5;
+mod = num3%num4;
+
+let max = Math.max(5, 18, 20, 3, 12);
